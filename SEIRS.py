eb20d2c59553562442a1528e19f100e564ef27db
柳洋 <yangliu2@caltech.edu>
2020-04-07 20:54:33 -0700

    add initial_parameters for SEIR model

--- SEIRS.py
+++ SEIRS.py
@@ -1,10 +1,12 @@
 from seirsplus.models import *
 from preprocesss import PreProcess
+import pandas as pd
+from collections import Counter
 
 
 class SEIRSModelClass(object):
     def __init__(self):
-        self.model = None
+        self.models = []
         self.data = None
         self.checkpoints = {
             't': [50, 100],
@@ -12,42 +14,61 @@ class SEIRSModelClass(object):
             'theta_E': [0.02, 0.02],
             'theta_I': [0.02, 0.02]
         }
+        self.initial_parameters = []
+        self.period = 300
 
     def preprocess(self):
         preprocess = PreProcess('./data/us/covid/')
         countyInfo = preprocess.deathData[['countyFIPS', 'County Name', 'State', 'stateFIPS']]
-        print(countyInfo['countyFIPS'])
+        county_population = pd.read_csv('data/us/demographics/county_populations.csv')
 
+        county_initial_parameters = county_population.merge(countyInfo, left_on='FIPS', right_on='countyFIPS')
+        county_numbers = len(county_initial_parameters)
+        beta = pd.Series([0.147 for _ in range(county_numbers)], name='beta')
+        sigma = pd.Series([1 / 5.2 for _ in range(county_numbers)], name='sigma')
+        gamma = pd.Series([1 / 12.39 for _ in range(county_numbers)], name='gamma')
+        mu_I = pd.Series([0.001 for _ in range(county_numbers)], name='mu_I')
+        xi = pd.Series([0 for _ in range(county_numbers)], name='xi')
+        sigma_D = pd.Series([1 / 5.2 for _ in range(county_numbers)], name='sigma_D')
+        theta_E = pd.Series([0 for _ in range(county_numbers)], name='theta_E')
+        theta_I = pd.Series([0 for _ in range(county_numbers)], name='theta_I')
+        psi_E = pd.Series([0 for _ in range(county_numbers)], name='psi_E')
+        psi_I = pd.Series([0 for _ in range(county_numbers)], name='psi_I')
+
+        self.initial_parameters = pd.concat([county_initial_parameters, beta, sigma, gamma, mu_I, xi, sigma_D, theta_E, theta_I, psi_E, psi_I], axis=1)
+        self.initial_parameters.to_csv('processed_data/county_initial_parameters.csv', index=False)
 
     def train(self):
-        # self.model = SEIRSModel(beta=0.155, sigma=1/5.2, gamma=1/12.39, initN=100000, initI=100)
-        self.model = SEIRSModel(
-            initN=1000000,
-            beta=0.147,         # transmissions per S-I contact per time
-            sigma=1 / 5.2,      # inverse of incubation period
-            gamma=1 / 12.39,    # inverse of infectious period
-            mu_I=0.001,         # deaths per infectious individual per time
-            mu_0=0,
-            nu=0,
-            xi=0,               # 0: inverse of temporary immunity period
-            beta_D=0.147,
-            sigma_D=1 / 5.2,    # rate of progression for detected cases
-            gamma_D=1 / 12.39,
-            mu_D=0.0004,
-            theta_E=0,          # rate of testing for exposed individuals
-            theta_I=0,          # rate of testing for infectious individuals
-            psi_E=1.0,          # rate of positive test results for exposed individuals
-            psi_I=1.0,          # rate of positive test results for infectious individuals
-            initI=10000,
-            initE=0,
-            initD_E=0,
-            initD_I=0,
-            initR=0,
-            initF=0
-        )
+        for i in range(len(self.initial_parameters)):
 
-        self.model.run(T=300, checkpoints=self.checkpoints)
-        # self.model.run(T=300)
+            model = SEIRSModel(
+                initN=self.initial_parameters['total_pop'][i],
+                beta=self.initial_parameters['beta'][i],         # transmissions per S-I contact per time
+                sigma=self.initial_parameters['sigma'][i],      # inverse of incubation period
+                gamma=self.initial_parameters['gamma'][i],    # inverse of infectious period
+                mu_I=self.initial_parameters['mu_I'][i],         # deaths per infectious individual per time
+                mu_0=0,
+                nu=0,
+                xi=self.initial_parameters['xi'][i],               # 0: inverse of temporary immunity period
+                beta_D=0.147,
+                sigma_D=self.initial_parameters['sigma_D'][i],    # rate of progression for detected cases
+                gamma_D=1 / 12.39,
+                mu_D=0.0004,
+                theta_E=self.initial_parameters['theta_E'][i],          # rate of testing for exposed individuals
+                theta_I=self.initial_parameters['theta_I'][i],          # rate of testing for infectious individuals
+                psi_E=self.initial_parameters['psi_E'][i],          # rate of positive test results for exposed individuals
+                psi_I=self.initial_parameters['psi_I'][i],          # rate of positive test results for infectious individuals
+                initI=10000,
+                initE=0,
+                initD_E=0,
+                initD_I=0,
+                initR=0,
+                initF=0
+            )
+
+            model.run(T=self.period, checkpoints=self.checkpoints)
+
+            self.models.append(model)
 
     def visualization(self):
         self.model.figure_infections(vlines=self.checkpoints['t'], plot_F='line', ylim=0.2)
@@ -57,4 +78,4 @@ if __name__ == '__main__':
     seirsModel = SEIRSModelClass()
     seirsModel.preprocess()
     seirsModel.train()
-    seirsModel.visualization()
+    # seirsModel.visualization()
