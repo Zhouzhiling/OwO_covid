7114f368b24e9c2d864e65bad2cd8dae0abbe865
柳洋 <yangliu2@caltech.edu>
2020-04-09 16:14:32 -0700

    add methods implementation

--- output.py
+++ output.py
@@ -4,13 +4,20 @@ import pandas as pd
 class Output(object):
 
     def __init__(self):
+        self.sample = self.read_sample()
         pass
 
-    def read_sample(self):
-        pass
+    @staticmethod
+    def read_sample():
+        return pd.read_csv('sample_submission.csv')
 
     def modify_submission(self):
         pass
 
     def save_submission(self):
         pass
+
+
+if __name__ == '__main__':
+    output = Output()
+    output.save_submission()
