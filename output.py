3956523634ba9deb8e92b49f2e12cb8333e36f44
柳洋 <yangliu2@caltech.edu>
2020-04-09 16:12:20 -0700

    add output structure

---
+++ output.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+class Output(object):
+
+    def __init__(self):
+        pass
+
+    def read_sample(self):
+        pass
+
+    def modify_submission(self):
+        pass
+
+    def save_submission(self):
+        pass
