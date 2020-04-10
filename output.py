f5906a5573c695e6a1df580d2ccec1b5995989a8
柳洋 <yangliu2@caltech.edu>
2020-04-09 20:17:04 -0700

    add format

--- output.py
+++ output.py
@@ -5,7 +5,6 @@ class Output(object):
 
     def __init__(self):
         self.sample = self.read_sample()
-        pass
 
     @staticmethod
     def read_sample():
@@ -14,7 +13,23 @@ class Output(object):
     def modify_submission(self):
         pass
 
+    def format_key(self, key):
+        strs = key.split('-')
+        year = strs[0]
+        month = strs[1][1]
+        if strs[2][0] == '0':
+            day = strs[2][1]
+        else:
+            day = strs[2]
+
+        date = month + '/' + day + '/' + year
+
+        FIPS = strs[3]
+
+        return date, FIPS
+
     def save_submission(self):
+        self.modify_submission()
         pass
 
 
