99bda4a5ebdc7d6ac5f2f121be1b3b23b9ffa3e3
ling <zhilingzhouzju@gmail.com>
2020-04-09 21:16:48 -0700

    fix conflict:)))

--- output.py
+++ output.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import datetime
 
 class Output(object):
 
@@ -11,11 +11,39 @@ class Output(object):
     def read_sample():
         return pd.read_csv('sample_submission.csv')
 
+    def generate_key(self, cur_date, FIPS):
+        # e.g. 2020-04-01-10001
+        return cur_date.strftime('%Y-%m-%d') + '-' + str(int(FIPS))
+        
+
     def modify_submission(self):
         # predicted part
         predicted = pd.read_csv('processed_data/SEIRS_predictions.csv')
-        for deaths in predicted.values:
-            FIPS, deaths = deaths[0], deaths[1:]
+        date_time = datetime.datetime.strptime(self.last_day, '%m/%d/%Y')
+        key_value = dict()
+
+        for infos in predicted.values:
+            FIPS, deaths = infos[0], infos[1:]
+            for day_idx, death in enumerate(deaths):
+                if day_idx == 0:
+                    continue
+                cur_date = date_time + datetime.timedelta(days=day_idx)
+                key = self.generate_key(cur_date, FIPS)
+                value = self.generate_percentile(death)
+                key_value[key] = value
+                # for i, v in enumerate(value):
+                # index = self.sample['id'] == key
+                # self.sample.loc[][str((i+1)*10)] = v
+                # print(key)
+                # print(self.sample.loc[self.sample['id'] == key][str((i+1)*10)])
+
+        for i in range(len(self.sample.values)):
+            key = self.sample.values[i][0]
+            if key not in key_value:
+                continue
+            percentiles = key_value[key]
+            for j in range(len(percentiles)):
+                self.sample.iloc[i, j] = percentiles[j]
 
         # ground truth part
         ground_truth = pd.read_csv('data/us/covid/deaths.csv')
@@ -29,6 +57,7 @@ class Output(object):
             percentile = self.generate_percentile(average)
             for j in range(9):
                 self.sample.iloc[i, j + 1] = percentile[j]
+
         
     @staticmethod
     def generate_percentile(mid):
