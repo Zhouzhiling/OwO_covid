38e9025c3723967de3a2a0592a9cb9b470ace54e
柳洋 <yangliu2@caltech.edu>
2020-04-09 20:54:13 -0700

    modify sample submission in-place with ground_truth

--- output.py
+++ output.py
@@ -16,17 +16,30 @@ class Output(object):
         predicted = pd.read_csv('processed_data/SEIRS_predictions.csv')
         for deaths in predicted.values:
             FIPS, deaths = deaths[0], deaths[1:]
+
+        # ground truth part
+        ground_truth = pd.read_csv('data/us/covid/deaths.csv')
+        for i in range(len(self.sample)):
+            date, FIPS = self.format_key(self.sample['id'][i])
+            if date not in ground_truth or int(FIPS) not in ground_truth['countyFIPS']:
+                continue
+            if len(ground_truth.loc[ground_truth['countyFIPS'] == int(FIPS)][date].values) == 0:
+                continue
+            average = ground_truth.loc[ground_truth['countyFIPS'] == int(FIPS)][date].values[0]
+            percentile = self.generate_percentile(average)
+            for j in range(9):
+                self.sample.iloc[i, j + 1] = percentile[j]
         
-    def generate_percentile(self, mid):
-        length = 9
-        output = []
+    @staticmethod
+    def generate_percentile(mid):
+        percentile = []
         unit = mid / 5.0
         for i in range(9):
-            output.append(unit * (i+1))
-        return output
+            percentile.append(unit * (i+1))
+        return percentile
 
-
-    def format_key(self, key):
+    @staticmethod
+    def format_key(key):
         strs = key.split('-')
         year = strs[0]
         month = strs[1][1]
@@ -43,11 +56,11 @@ class Output(object):
 
     def save_submission(self):
         self.modify_submission()
-        pass
+        self.sample.to_csv('submission.csv')
 
 
 if __name__ == '__main__':
     output = Output()
-    # output.save_submission()
+    output.save_submission()
     output.modify_submission()
     # print(output.generate_percentile(5))
