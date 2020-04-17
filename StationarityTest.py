c0b76463a48830aa55e78ffce1f47f3db74c0253
柳洋 <yangliu2@caltech.edu>
2020-04-16 21:46:31 -0700

    add stationary test

---
+++ StationarityTest.py
@@ -0,0 +1,43 @@
+import statsmodels
+from statsmodels.tsa.stattools import adfuller
+import pandas as pd
+
+
+class StationarityTest(object):
+
+    def __init__(self):
+        self.data = None
+        self.adfs = []
+        self.county_number = 0
+        self.significance = 0.05
+        self.is_stationary = []
+        pass
+
+    def load_data(self):
+        self.data = pd.read_pickle('processed_data/DTW/DTW_label_deaths.plk')
+        self.county_number = len(self.data)
+
+    def test(self):
+        self.load_data()
+
+        for i in range(self.county_number):
+            if len(self.data['death_list'][i]) <= 3:
+                self.is_stationary.append(True)
+                continue
+            adf = adfuller(self.data['death_list'][i], autolag='AIC')
+            if adf[1] < self.significance:
+                self.is_stationary.append(True)
+            else:
+                self.is_stationary.append(False)
+            self.adfs.append(adf)
+
+        self.data = pd.concat([self.data, pd.Series(data=self.is_stationary, name='stationary')], axis=1)
+
+    def output(self):
+        self.data.to_pickle(path='processed_data/Stationary/stationary_label_deaths.plk')
+
+
+if __name__ == '__main__':
+    stationarity = StationarityTest()
+    stationarity.test()
+    pass
