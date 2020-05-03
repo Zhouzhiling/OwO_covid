9c63b0cd44e141cdb0b798a5959ec416a40cab7b
柳洋 <yangliu2@caltech.edu>
2020-05-03 15:20:55 -0700

    add population and icu

--- preprocessForNN.py
+++ preprocessForNN.py
@@ -1,27 +1,35 @@
 import numpy as np
 import pandas as pd
+from collections import defaultdict
 
 
 class PreprocessForNN(object):
+
     def __init__(self):
+        self.icu_beds = defaultdict(int)
+        self.total_population = defaultdict(int)
         pass
 
     def load_data(self):
-        self.load_confirmed_dict()
+        self.load_death_and_confirmed()
         self.load_icu_dict()
         self.load_death_and_confirmed()
+        self.load_population_dict()
 
     def load_death_and_confirmed(self):
         pass
 
     def load_icu_dict(self):
-        # TODO:
-        # generate a dict, key = countyFIPS, value = icu_bed_count
-        pass
+        beds = pd.read_csv(filepath_or_buffer='data/us/hospitals/beds_by_county.csv')
+        for item in beds.iterrows():
+            self.icu_beds[item[1]['FIPS']] = item[1]['icu_beds']
 
     def load_population_dict(self):
         # TODO:
         # generate a dict, key = countyFIPS, value = population
+        population = pd.read_csv(filepath_or_buffer='data/us/demographics/county_populations.csv')
+        for item in population.iterrows():
+            self.total_population[item[1]['FIPS']] = item[1]['total_pop']
         pass
 
 
